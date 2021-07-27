17c1e244a6d0453654788a0be5a7b58425978fe3
Alex Chaglov <a.chaglov@gmail.com>
2021-07-27 15:58:36 +0500

    config files on

--- ha_gitpush.sh
+++ ha_gitpush.sh
@@ -6,7 +6,7 @@ cd /usr/share/hassio/homeassistant
 git add .
 
 # Commit changes with message with current date stamp
-git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
+git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S`"
 
 # Push changes towards GitHub
 git push -u origin master
